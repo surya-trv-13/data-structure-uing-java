024e9068cd839892172a42b556434f7cdc23b0e3
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2022-09-18 21:47:08 +0530

    Search BST | enhancement

--- src/com/dsa/surya/bst/BinarySearchTree.java
+++ src/com/dsa/surya/bst/BinarySearchTree.java
@@ -8,9 +8,16 @@ public class BinarySearchTree {
 		LinkedList linkedList = new LinkedList();
 		linkedList.createBst();
 		linkedList.inOrderTraversal(linkedList.head);
-		System.out.println("Enter the number you want to search");
-		int number = scanner.nextInt();
-		boolean result = linkedList.searchData(number);
-		System.out.println("The value in the search result is "+result);
+
+		String choice; 
+		do {
+			System.out.println("Enter the number you want to search");
+			int number = scanner.nextInt();
+			boolean result = linkedList.searchData(number);
+			System.out.println("The value in the search result is "+result);
+
+			System.out.println("Do you want to search more?");
+			choice = scanner.next();
+		} while("y".equals(choice) || "Y".equals(choice));
 	}
 }
